18d42f0b03e54f677036e5feed5b37008f9959da
Nick Borrello <nvborrello@wpi.edu>
2023-09-15 14:21:38 -0400

    Update, add, and remove contacts

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,55 @@
-// FRONT-END (CLIENT) JAVASCRIPT HERE
 window.onload = function () {
-  const name = document.querySelector("#expenseName"),
-    number = document.querySelector("#expenseNumber"),
-    photo = document.querySelector("#expenseDate");
+  // On load update the table
 
-  const button = document.querySelector("#createButton");
-  button.onclick = create;
+  updateContacts();
 
-  document.querySelector("#expenseDate").valueAsDate = new Date();
-
-  updateExpenses();
 };
+
+const updateContacts = async function () {
+
+  // Clear the expenses
+  let contactList = document.getElementById("contactList");
+  contactList.innerHTML = '';
+
+  // GET the current expenses
+  const response = await fetch("/update", {
+    method: "GET",
+  });
+
+
+  const contacts = await response.json();
+  console.log(contacts);
+};
+
+const addContact = async function () {
+  // Get the data from the form
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+  const email = document.getElementById("email").value;
+
+  // POST the data to the server
+  const response = await fetch("/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, phone, email }),
+  });
+
+  // Update the table
+  updateContacts();
+}
+
+const removeContact = async function (id) {
+  // POST the data to the server
+  const response = await fetch("/remove", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ _id: id }),
+  });
+
+  // Update the table
+  updateContacts();
+}
